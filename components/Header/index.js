47bd1526d43420dc62c02d35372829d35dfdb5c2
Ike Otu <ikeotu@msn.com>
2020-01-17 10:48:04 -0800

    Header component complete, static data still needs to be passed

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,11 +2,52 @@
 // -----------------------
 // Using a function create the component you see below:
 //
-//  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
-//    <h1>Lambda Times</h1>
-//    <span class="temp">98°</span>
+//  <div class="header"> created with class
+//    <span class="date">SMARCH 28, 2019</span> created
+//    <h1>Lambda Times</h1> created
+//    <span class="temp">98°</span> created
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const staticData = [
+    {
+        Date: `March 28, 2019`,
+        Temp: `98°`
+    }
+]
+
+function Header(Date, Temp) {
+    // create components
+    const parentDiv = document.createElement('div');
+    const dateSpan = document.createElement('span');
+    const h1Element = document.createElement('h1');
+    const tempSpan = document.createElement('span');
+
+        
+    // Add classes for created elements
+    parentDiv.classList.add('header')
+    dateSpan.classList.add('date');
+    tempSpan.classList.add('temp');
+
+    // Append created elements to the containing parentDiv
+    parentDiv.append(dateSpan);
+    parentDiv.append(h1Element); 
+    parentDiv.append(tempSpan);
+
+    //Add text to requisite elements
+    dateSpan.textContent = Date;
+    h1Element.textContent = `Lambda Times`;
+    tempSpan.textContent = Temp;
+
+    return parentDiv;
+}
+
+// selected class from html that i'm going to append the new component to
+const mainHTMLAppend = document.querySelector('.header-container');
+
+staticData.map((newComp) => {
+    mainHTMLAppend.append(Header(newComp.Date, newComp.Temp));
+})
+
+//append to that class in the main HTML
+mainHTMLAppend.append(parentDiv);
