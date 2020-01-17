3ac75306360489f26e906aae1021ded5158cf73a
Ike Otu <ikeotu@msn.com>
2020-01-17 13:30:49 -0800

    tabs component complete

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,43 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+function tabComponent (obj){
+    // create tab component
+    const tabDiv = document.createElement('div');
+
+    //add class of tab
+    tabDiv.classList.add('tab');
+
+    //pass obj data to tab
+    tabDiv.textContent = obj.topics;
+
+    return tabDiv;
+
+}
+
+// selected class from html that i'm going to append the new component to
+const appendNewTab = document.querySelector('.topics');
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+     const makeNewTab = tabComponent(response.data);
+        appendNewTab.appendChild(makeNewTab);
+
+    })
+    .catch(error => {
+        console.log("An error has occurred", error);
+      })
+
+// this forEach now grabs the data from the API via the Axios call and appends to the main HTML
+data.forEach(event => {
+    axios.get(`https://lambda-times-backend.herokuapp.com/${event}`)
+      .then(response => {
+        const tabData = newCard(response.data.topics);
+        appendNewTab.appendChild(tabData);
+      })
+      .catch (error => {
+          console.log("An error has occurred", error);
+      })
+  })     
